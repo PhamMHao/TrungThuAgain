a9c9109c052313029e69fcff85c223a77b7716b6
Pham Minh Hao <phammhao@outlook.com>
2024-08-11 14:15:13 +0700

    update code

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bạn bên đó dễ thương ! 🖐🖐🖐',
+    introDesc: `Tui có cái này hay cực luôn nè, nhấn vô để biết nhé !!😍😍`,
+    btnIntro: 'OK',
+    title: 'có thật là trung thu này hk mún đi chơi với tớ đúng hk',
+    desc: 'trả lời thật lòng nha, khum out ấy nhá heo:>😠😠😠 ',
+    btnYes: 'hiển nhiên là sẽ đi rồi😘😘',
+    btnNo: 'khummmmmm,🤧🤧🤧 tui có hẹn lịch khác rồi ',
+    question:'Tui biết mà kiểu giề cũng sẽ chấp nhận đi với tui mà',
+    reasonPlaceholder: 'Lí do là vì sao dọ',
+    btnReply: 'Gởi ! ',
+    reply: 'đi âu cũng được, miễn có nhau là được',
+    mess: 'hẹn ai đó vào dịp Trung thu nhé ♥️',
+    messDesc: 'nhanh tay inbox luôn để có thêm thông tin nha. 🐷🐷🐷',
+    btnAccept: 'Ok lunnn :v',
+    messLink: 'https://www.facebook.com/profile.php?id=100042290842495&mibextid=ZbWKwL'
+}
